3df49136f34d999b94e1e050f3ba0996d8d372a5
youngdong82 <nfs82young@gmail.com>
2022-07-14 14:44:20 +0900

    ✨ app.py

---
+++ app.py
@@ -0,0 +1,151 @@
+# 서버 구조
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient # DB 관련
+import certifi # DB 보완문제 해결
+# 암호화
+import jwt
+import hashlib
+
+# 환경변수
+from dotenv import dotenv_values
+
+# 시간 관련 라이브러리
+from datetime import datetime, timedelta
+
+# 환경변수
+config = dotenv_values(".env")
+SECRET_KEY = config['JWT_SECRET_KEY']
+MONGO_ID = config['MONGO_ID']
+MONGO_PW = config['MONGO_PW']
+
+# 서버 시작
+app = Flask(__name__)
+# DB 연결
+ca = certifi.where()
+client = MongoClient(f'mongodb+srv://{MONGO_ID}:{MONGO_PW}@cluster0.sj0x8tf.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.error_note
+
+# --------------------------------------- 페이지 렌더링
+# 첫 페이지 by youngdong
+@app.route('/')
+def render_index():
+    token_receive = request.cookies.get('mytoken')
+    print(token_receive)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+# 회원가입 페이지 by youngdong
+@app.route('/register')
+def render_register():
+  msg = request.args.get("msg")
+  return render_template('register.html', msg=msg)
+
+# 로그인 페이지 by youngdong
+@app.route('/login')
+def render_login():
+  msg = request.args.get("msg")
+  return render_template('login.html', msg=msg)
+
+
+# 에러_템플릿 작성 페이지 by siwon
+@app.route('/detail')
+def detail():
+    error = list(db.errors.find({}, {'_id': False}))
+    return render_template("detail.html", error=error)
+    
+    
+# 에러_템플릿 디테일 by siwon
+@app.route('/detail1')
+def detail1():
+    errors = list(db.error.find({}, {'_id': False}))
+    return render_template("detail1.html", errors=errors)
+
+
+
+# --------------------------------------- 액션
+# 로그인하기 by youngdong
+@app.route('/sign_in', methods=['POST'])
+def do_login():
+    # 로그인
+    loginId_receive = request.form['loginId_give']
+    loginPw_receive = request.form['loginPw_give']
+
+    pw_hash = hashlib.sha256(loginPw_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'userId': loginId_receive, 'pw': pw_hash})
+
+    if result is not None:
+        createdAt = datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        payload = {
+            'id': loginId_receive,
+            'createdAt': str(createdAt)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+# 회원가입하기/회원 정보 저장하기 by youngdong
+@app.route('/sign_up/save', methods=['POST'])
+def do_sign_up():
+  userId_receive = request.form["userId_give"]
+  pw_receive = request.form["pw_give"]
+  pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+  doc = {
+    "userId": userId_receive,
+    "pw": pw_hash,
+  }
+  db.users.insert_one(doc)
+  return jsonify({'result': 'success'})
+
+# 아이디 중복여부 확인하기 by youngdong
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+  userId_receive = request.form['userId_give']
+  exists = bool(db.users.find_one({"userId": userId_receive}))
+  return jsonify({'result': 'success', 'exists': exists})
+  
+  
+  
+# 에러_템플릿 디테일 받아오기 by siwon
+@app.route("/errors", methods=["GET"])
+def error_get():
+    messages = []
+    errors_list = list(db.error.find())
+    for error in errors_list:
+        messages.append(error['message'])
+    return jsonify({'messages': messages})
+
+# 에러_템플릿 작성하기 by siwon
+@app.route("/error_post", methods=["POST"])
+def error_post():
+    message_receive = request.form['message_give']
+    language_receive = request.form['language_give']
+    state_receive = request.form['state_give']
+    solution_receive = request.form['solution_give']
+    note_receive = request.form['note_give']
+    link_receive = request.form['link_give']
+
+    doc = {
+        'message': message_receive,
+        'language': language_receive,
+        'state': state_receive,
+        'solution': solution_receive,
+        'note': note_receive,
+        'link': link_receive
+    }
+    db.error.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '작성 완료!'})
+
+
+# 포트
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5500, debug=True)
