3effe4b099fa3293f5a005145365de4420908f93
youngdong82 <nfs82young@gmail.com>
2022-07-15 12:19:10 +0900

    ➕ show user_id, time

--- static/index/index.js
+++ static/index/index.js
@@ -1,3 +1,25 @@
+const get_diff_time = (createdAt) => {
+  const now = Date.now();
+  const diff = Date.now() - createdAt
+  const standard_diff = parseInt(diff / 1000)
+
+  let final_diff = 0
+  if (standard_diff < 60){
+    final_diff = `${standard_diff} 초 전`
+  } else if(standard_diff < 60* 60){
+    final_diff = `${parseInt(standard_diff/60)}분 전`
+  } else if(standard_diff < 60* 60 * 24){
+    final_diff = `${parseInt(standard_diff/(60*60))}시간 전`
+  } else if(standard_diff < 60* 60 * 24 * 30){
+    final_diff = `${parseInt(standard_diff/(60*60*24))}일 전`
+  } else if(standard_diff < 60* 60 * 24 * 30 * 12){
+    final_diff = `${parseInt(standard_diff/(60*60*24*30))}개월 전`
+  } else{
+    final_diff = `${parseInt(standard_diff/(60*60*24*30*12))}년 전`
+  }
+  return final_diff
+}
+
 function get_posts(username) {
   if (username == undefined) {
     username = ""
@@ -13,6 +35,10 @@ function get_posts(username) {
 
         for (let i = 0; i < posts.length; i++) {
           let post = posts[i]
+          let user_id = post["user_id"]
+          let created_at = parseInt(post["created_at"])
+          const diff_time = get_diff_time(created_at)
+
           let errorId = post["_id"]
           let error_msg = post["message"]
           let error_lang= post["language"]
@@ -24,7 +50,7 @@ function get_posts(username) {
             `
               <div class="content">
                 <div class="content_title">
-                  <span class="paint_red"> ${error_msg} </span> <span>@username</span> <span>10분 전</span>
+                  <span class="paint_red"> ${error_msg} </span> <span>@${user_id}</span> <span>${diff_time}</span>
                 </div>
                 <div class="content_lang"><span>${error_lang}</span></div>
                 <p class="content_solution paint_green">
