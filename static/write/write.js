50819dae696853faa08b470c35fc63bc78b586bb
youngdong82 <nfs82young@gmail.com>
2022-07-18 18:14:14 +0900

    ✨select_box in write.js

--- static/write/write.js
+++ static/write/write.js
@@ -1,6 +1,19 @@
 function errors_post() {
+  let selected_lang
+  const lang_select = document.querySelector('#lang_select');
+  for (let i=0; i<lang_select.length; i++){
+    const each_lang_select = lang_select[i]
+
+    if(each_lang_select.selected === true){
+      selected_lang = each_lang_select.value
+      if(selected_lang === ''){
+        alert('언어를 선택해주세요.')
+        return
+      }
+      break
+    }
+  }
   let message = $("#message").val();
-  let language = $("#language").val();
   let state = $("#state").val();
   let solution = $("#solution").val();
   let note = $("#note").val();
@@ -12,7 +25,7 @@ function errors_post() {
     data: {
       createdAt: Date.now(),
       message_give: message,
-      language_give: language,
+      language_give: selected_lang,
       state_give: state,
       solution_give: solution,
       note_give: note,
@@ -23,8 +36,4 @@ function errors_post() {
       window.location.href = "/";
     },
   });
-}
-
-function close_box() {
-  $("#errornote").hide();
 }
